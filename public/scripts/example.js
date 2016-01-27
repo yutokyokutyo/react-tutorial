44522f173116cede86eae29d59f74dc84f2961a4
yutokyokutyo <k.yuto.cre@gmail.com>
2016-01-27 21:18:38 +0900

    勉強会 DONE

---
+++ public/scripts/example.js
@@ -0,0 +1,108 @@
+var CommentForm = React.createClass({
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var author = ReactDOM.findDOMNode(this.refs.author).value.trim();
+        var text = ReactDOM.findDOMNode(this.refs.text).value.trim();
+        if (!text || !author) {
+            return;
+        }
+        this.props.onCommentSubmit({author: author, text: text});
+        ReactDOM.findDOMNode(this.refs.author).value = '';
+        ReactDOM.findDOMNode(this.refs.text).value = '';
+        return;
+    },
+    render: function() {
+        return (
+            <form className="commnetForm" onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="kyokutyo" ref="author" />
+            <input type="text" placeholder="ファイナルビックバン" ref="text" />
+            <input type="submit" value="Post" />
+            </form>
+           );
+    }
+});
+
+var Comment = React.createClass({
+    render: function() {
+        return (
+                <div className="comment">
+                <h2 className="commentAuthor">
+                {this.props.author}
+                </h2>
+                {this.props.children}
+                </div>
+               );
+    }
+});
+
+
+var data = [
+{author: "局長", text: "hogehoge"},
+{author: "ネッシー", text: "i am nessy"}
+];
+
+var CommentBox = React.createClass({
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    handleCommentSubmit: function(comment) { 
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            type: 'POST',
+            data: comment,
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    render: function() {
+        return (
+                <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.state.data} />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+                </div>
+               );
+    }
+});
+
+
+var CommentList = React.createClass({
+    render: function() {
+        var commentNodes = this.props.data.map(function (comment) {
+            return (
+                    <Comment author={comment.author}>
+                    {comment.text}
+                    </Comment>
+                   );
+        });
+        return (
+                <div className="commentList">
+                {commentNodes}
+                </div>
+               );
+    }
+});
+
+ReactDOM.render(
+        <CommentBox url="api/comments" />,
+        document.getElementById('content')
+        );
+
